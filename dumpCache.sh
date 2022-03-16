f5517a28cfc10882447356ee525e8520186249c1
Ammar Zakwan <79646199+ammarzakwan-cell@users.noreply.github.com>
2022-03-16 13:07:44 +0800

    Add files via upload

---
+++ dumpCache.sh
@@ -0,0 +1,1 @@
+composer dump-autoload && php artisan clear-compiled && php artisan config:cache && php artisan cache:clear && php artisan view:clear && php artisan config:clear && php artisan route:clear && php artisan optimize
